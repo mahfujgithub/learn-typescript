842f4d197cd99c5b1738218452ee0652b5d12f96
mahfujgithub <mahfujurrahmanofficial23@gmail.com>
2024-08-23 06:41:10 +0600

    array & tupple

---
+++ src/array.ts
@@ -0,0 +1,11 @@
+// String array type
+const names:string[] = ["Mahfuj", "Rikta", "Noman"];
+
+// Number array type
+
+const ids:number[] = [45, 84, 87];
+
+// Two mix type called Tupple in TypeScript
+
+const user: [number, string] = [45, "Mahfuj"];
+
